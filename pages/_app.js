60ecd904885804908e1fa609126600e0d8ae9f66
delinuxist <delinuxist@gmail.com>
2022-06-29 23:49:11 +0000

    imported navbar component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import Navbar from '../components/utilities/layouts/Navbar';
 import { Provider } from '../context';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider>
+      <Navbar />
       <Component {...pageProps} />
     </Provider>
   );
