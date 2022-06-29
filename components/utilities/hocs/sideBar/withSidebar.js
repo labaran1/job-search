1a1fa937d1d0b4cab1e21c79a0eb56cc2f21fb70
labaran1 <adlabaran@gmail.com>
2022-06-29 22:27:07 +0000

    sidebar structure

---
+++ components/utilities/hocs/sideBar/withSidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from '../../components/sidebar';
+export default (WrappedComponent, pages) => {
+  console.log(pages);
+  const hocComponent = ({ ...props }) => (
+    <div style={{ display: 'flex' }}>
+      <Sidebar />
+      <WrappedComponent {...props} />;
+    </div>
+  );
+
+  return hocComponent;
+};
