3c35ee44ffeed1700b35d62cc8cd0c65b01dae47
delinuxist <delinuxist@gmail.com>
2022-06-29 16:43:16 +0000

    changed const to let

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 
 const errorHandler = (err, req, res, next) => {
   console.log(err);
-  const customErr = {
+  let customErr = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something broke',
   };
