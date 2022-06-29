014707acf19bc52d383918d6901cff8ea045697a
labaran1 <adlabaran@gmail.com>
2022-06-29 23:59:11 +0000

    user Info

--- components/utilities/components/sidebar/index.js
+++ components/utilities/components/sidebar/index.js
@@ -1,26 +1,44 @@
 import React, { useContext } from 'react';
 import { Context } from '../../../../context/index';
+import Link from 'next/link';
+
+import { AiFillSetting, AiOutlineUser } from 'react-icons/ai';
 export default function Sidebar() {
   const appContext = useContext(Context);
   const { boards, user } = appContext.state;
   return (
-    <nav>
-      <div> User Info</div>
-      <div>
-        <ul>
-          <li>Jobs</li>
-
-          <li>
-            Boards
-            <ul>
-              {boards.map((b) => (
-                <li key={b.id}>{b.name}</li>
-              ))}
-            </ul>
-          </li>
-          <li>Metrics</li>
-        </ul>
+    <nav className="bg-slate-50  w-72 h-screen ml-2 mt-10">
+      <div className="flex  bg-slate-200 justify-evenly  rounded h-9 w-52 ml-8">
+        {' '}
+        <AiOutlineUser className="mt-2 text-xl" />
+        <span className="text-xl mt-1">
+          {user?.name.charAt(0).toUpperCase() + user?.name.slice(1)}
+        </span>
+        <AiFillSetting
+          className=" cursor-pointer mt-2 text-xl"
+          title="Go to settings"
+        />
       </div>
+
+      <ul>
+        <Link href={`/user/${user?.name}/jobs`}>
+          <a>Jobs</a>
+        </Link>
+
+        <li>
+          Boards
+          <ul>
+            {boards.map((b) => (
+              <Link href={`/user/${user?.name}/boards/${b.id}`} key={b.id}>
+                <a> {b.name}</a>
+              </Link>
+            ))}
+          </ul>
+        </li>
+        <Link href={`/user/${user?.name}/reports`}>
+          <a>Reports</a>
+        </Link>
+      </ul>
     </nav>
   );
 }
