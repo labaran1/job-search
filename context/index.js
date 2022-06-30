0941c45785ebcaf09b478385cf4742883b42306b
delinuxist <delinuxist@gmail.com>
2022-06-30 00:47:05 +0000

    get data from localStorage

--- context/index.js
+++ context/index.js
@@ -1,6 +1,7 @@
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
 import { useReducer } from 'react';
 import rootReducer from './rootReducer';
+import { LOGIN } from './types';
 
 // context
 const Context = createContext();
@@ -12,6 +13,15 @@ const initialState = {
 // provider
 const Provider = ({ children }) => {
   const [state, dispatch] = useReducer(rootReducer, initialState);
+
+  // get data from localStorage to context
+  useEffect(() => {
+    dispatch({
+      type: LOGIN,
+      payload: JSON.parse(window.localStorage.getItem('user')),
+    });
+  }, []);
+
   return (
     <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
   );
